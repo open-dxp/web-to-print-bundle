5b5c1082451ee8f0096dc77540872f08f193db70
open-dxp-stack <stack@opendxp.ch>
2025-12-05 16:05:00 +0100

    Fix phpstan

--- src/Processor/Gotenberg.php
+++ src/Processor/Gotenberg.php
@@ -118,10 +118,10 @@ class Gotenberg extends Processor
             $chromium = $chromium->pdf();
         } else {
             // gotenberg-php v1 BC Layer for unsupported methods in v2
-            if (isset($params['userAgent']) && method_exists($chromium, 'userAgent')) {
+            if (isset($params['userAgent']) && method_exists($chromium, 'userAgent')) { // @phpstan-ignore-line
                 $chromium->userAgent($params['userAgent']);
             }
-            if (isset($params['pdfFormat'])&& method_exists($chromium, 'pdfFormat')) {
+            if (isset($params['pdfFormat'])&& method_exists($chromium, 'pdfFormat')) { // @phpstan-ignore-line
                 $chromium->pdfFormat($params['pdfFormat']);
             }
         }
