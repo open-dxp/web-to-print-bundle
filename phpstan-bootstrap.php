26490c862658a263b75a5c7e35ef687214b319d5
scrummer <scrummer@gmx.ch>
2026-02-12 15:13:55 +0100

    Refactor web-to-print bundle: update dependencies, adjust phpstan configurations, rename and simplify .php-cs-fixer configuration

--- phpstan-bootstrap.php
+++ phpstan-bootstrap.php
@@ -7,7 +7,7 @@ if (file_exists(__DIR__ . '/vendor/autoload.php')) {
 } elseif (getenv('OPENDXP_PROJECT_ROOT')) {
     define('OPENDXP_PROJECT_ROOT', getenv('OPENDXP_PROJECT_ROOT'));
 } else {
-    throw new \Exception('Unknown configuration! OpenDxp project root not found, please set env variable OPENDXP_PROJECT_ROOT.');
+    throw new \Exception('Unknown configuration! OpenDXP project root not found, please set env variable OPENDXP_PROJECT_ROOT.');
 }
 
 include OPENDXP_PROJECT_ROOT . '/vendor/autoload.php';
